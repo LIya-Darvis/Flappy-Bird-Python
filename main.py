bbb3ae7111765f373fde166f92178e86d19514a8
Liya Darvis <57328715+LIya-Darvis@users.noreply.github.com>
2023-02-15 17:48:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,26 @@ pipeimage = 'images/pipe.png'
 background_image = 'images/backround.jpg'
 birdplayer_image = 'images/bird.png'
 seavel_image = 'images/base.jfif'
+
+def flappygame():
+    your_score = 0
+    horizontal = int(window_width/5)
+    vertical = int(window_width/2)
+    ground = 0
+    mytempheight = 100
+    
+    first_pipe = createPipe()
+    second_pipe = createPipe()
+    
+def createPipe():
+    offset = window_height/3
+    pipeHeight = game_images['pipeimage'][0].get_height()
+    y2 = offset + \
+        random.randrange(0, int(window_height - game_images['sea_level'.get_height]() - 1.2 * offset))
+    pipeX = window_width + 10
+    y1 = pipeHeight - y2 + offset
+    pipe = [
+        {'x': pipeX, 'y': -y1}, 
+        {'x': pipeX, 'y': y2} 
+    ]
+    return pipe
