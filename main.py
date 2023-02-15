7cde56578a6015d29368770c36b39c140f3a220f
Liya Darvis <57328715+LIya-Darvis@users.noreply.github.com>
2023-02-15 17:36:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import pygame
+import random
+import sys
 
-print("---")
+window_width = 600
+window_height = 499
+
+window = pygame.display.set_mode((window_width, window_height))
+elevation = window_height * 0.8
+game_images = { }
+framepersecond = 32
+pipeimage = 'images/pipe.png'
+background_image = 'images/backround.jpg'
+birdplayer_image = 'images/bird.png'
+seavel_image = 'images/base.jfif'
